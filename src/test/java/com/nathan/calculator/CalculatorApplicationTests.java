ccb64dc3f2f958df8660ad5ca197b8f9b6a60983
NATER <165908372+kurapicu@users.noreply.github.com>
2025-10-23 14:27:30 +0100

    Add Spring Boot skeleton

---
+++ src/test/java/com/nathan/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
